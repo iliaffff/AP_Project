51878bee76641e387eee7d3fb2073a72d16a0420
Kiana <kianarjmnd@gmail.com>
2025-12-25 16:15:30 +0330

    complete accounts management UI

--- lib/screens/accounts/accounts_list_screen.dart
+++ lib/screens/accounts/accounts_list_screen.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
 import '../../data/fake_data.dart';
+import '../../models/account.dart';
 
 class AccountsListScreen extends StatelessWidget {
   const AccountsListScreen ({super.key});
 
+  IconData _getIcon(String type) {
+    return type == 'جاری' ? Icons.account_balance : Icons.savings;
+  }
+
   Widget build(BuildContext context) {
-    final accounts = FakeData.accounts;
+    final List<Account> accounts = FakeData.accounts;
     
     return Scaffold(
       appBar: AppBar(
-        title: const Text('حساب های من'),
+        title: const Text('مدیریت حساب ها'),
+        centerTitle: true,
       ),
       body: ListView.builder(
           itemCount: accounts.length,
           itemBuilder: (context, index) {
           final account = accounts[index];
           return Card(
-            margin: const EdgeInsets.all(12),
+            elevation: 3,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
             child: ListTile(
+              leading: Icon(
+                _getIcon(account.type),
+                size: 32,
+              ),
               title: Text(' حساب${account.type}'),
               subtitle: Text(account.id),
-              trailing: Text(
-                '${account.balance}تومان '
+              trailing: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    'موجودی',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    '${account.balance}تومان ',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
               onTap: () {
                 // بعدا جزئیات حساب رو اضافه میکنم بهش
